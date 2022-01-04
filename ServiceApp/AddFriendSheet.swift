f2a32c90e60b867176c51306f42e4e37a60a04a9
Mihir Chauhan <mihiravikam@gmail.com>
2022-01-03 23:52:10 -0800

    Added QRCode generator and reader

--- ServiceApp/AddFriendSheet.swift
+++ ServiceApp/AddFriendSheet.swift
@@ -6,25 +6,53 @@
 //
 
 import SwiftUI
+import CodeScanner
+import AudioToolbox
+
 
 struct AddFriendSheet: View {
     
     var body: some View {
         TabView {
-            Text("Your QR Code")
+            Image(uiImage: UIImage(data: generateQRCode(from: UUID().uuidString)!)!)
+                .resizable()
+                .frame(width: 290, height: 290, alignment: .center)
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "qrcode")
                 }
             
             
-            Text("Scan a QR Code")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "qrcode.viewfinder")
+            CodeScannerView(codeTypes: [.qr], simulatedData: "fakeUUID") { response in
+                switch response {
+                case .success(let result):
+                    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+                    print("Found code: \(result.string)")
+                    // add friend to firebase DB, when implemented.......
+                case .failure(let error):
+                    print(error.localizedDescription)
                 }
+            }
+            .font(.system(size: 30, weight: .bold, design: .rounded))
+            .tabItem {
+                Image(systemName: "qrcode.viewfinder")
+            }
         }
         
     }
     
+    func generateQRCode(from string: String) -> Data? {
+        let data = string.data(using: String.Encoding.ascii)
+        
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 10, y: 10)
+            
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output).pngData()!
+            }
+        }
+        
+        return nil
+    }
 }
