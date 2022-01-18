d7922b6d1f68742c5d6de831aff88387f510e436
Kelvin J <kelvin.jou@gmail.com>
2022-01-18 10:46:05 -0800

    Started on Firestore CRUD

--- ServiceApp/FirestoreCRUD.swift
+++ ServiceApp/FirestoreCRUD.swift
@@ -20,15 +20,22 @@ class FirestoreCRUD: ObservableObject {
                     return
                 } else {
                     for i in snap!.documentChanges {
+                        let host = i.document.get("host") as? String ?? "Host unavailable"
+                        let attendees = i.document.get("attendees") as? [String] ?? [String]()
                         let name = i.document.get("time") as? Date ?? Date()
                         let location = i.document.get("location") as? GeoPoint
-                        print(name, location?.latitude as Any, location?.longitude as Any)
+                        print(host, attendees, name, location?.latitude as Any, location?.longitude as Any)
                     }
                 }
             }
     }
     
     func AddToAttendeesList(eventID: String) {
-        db.collection("")
+        db.collection("Environment")
+            .document(eventID).updateData(["attendees" : FieldValue.arrayUnion([user_uuid])])
+    }
+    
+    func fetchUpdates() {
+        
     }
 }
