d52f37eab5d01b881a2fdd853aacfccc35aeaaa3
Kelvin <63611619+kelvinjou@users.noreply.github.com>
2022-02-02 10:36:13 -0800

    Added more data to Rec View

--- ServiceApp/RecommendedView.swift
+++ ServiceApp/RecommendedView.swift
@@ -11,18 +11,52 @@ import SDWebImageSwiftUI
 struct RecommendedView: View {
     @State var placeHolderImage = URL(string: "https://via.placeholder.com/150x150.jpg")
     var data: EventInformationModel
+    var dateToString: String {
+        get {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yyy"
+            let stringDate = dateFormatter.string(from: data.time)
+            return stringDate
+        }
+    }
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .center, vertical: .top)) {
             RoundedRectangle(cornerRadius: 20)
                 .frame(width: 180, height: 250)
                 .foregroundColor(Color(#colorLiteral(red: 0.9688304554, green: 0.9519491526, blue: 0.8814709677, alpha: 1)))
-            WebImage(url: self.placeHolderImage)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 180, height: 145)
-                .background(Color(.systemGray4))
-                .cornerRadius(20)
-                
+            VStack {
+                WebImage(url: self.placeHolderImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .background(Color(.systemGray4))
+                    .cornerRadius(20)
+                Text(data.name)
+                    .font(.headline)
+                    
+                Spacer()
+                HStack {
+                    Text(data.category)
+                        .bold()
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    Text(self.dateToString)
+                        .font(.caption)
+                        .foregroundColor(Color.gray)
+                }
+            }.frame(width: 180, height: 145)
+        //
+//        VStack(spacing: 0) {
+//            WebImage(url: self.placeHolderImage)
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 180, height: 145)
+//                .background(Color(.systemGray4))
+//                .cornerRadius(20)
+//            Rectangle()
+//                .frame(width: 180, height: 220)
+//                .cornerRadius(20, corners: [.topLeft, .topRight])
+//                .foregroundColor(Color(#colorLiteral(red: 0.9688304554, green: 0.9519491526, blue: 0.8814709677, alpha: 1)))
+//        }
                 .onAppear {
                     FIRCloudImages().getRemoteImages { connectionResult in
                         switch connectionResult {
@@ -34,7 +68,9 @@ struct RecommendedView: View {
                         }
                     }
                 }
-        }    }
+        }   }
 }
+
+
 
 
