5aa7eca4cf06bfa74ec2887d56a0ee3adca6794d
Mihir Chauhan <mihiravikam@gmail.com>
2022-02-22 11:03:29 -0800

    almost done with apple sign in

--- ServiceApp/AuthSignIn.swift
+++ ServiceApp/AuthSignIn.swift
@@ -8,6 +8,10 @@ import SwiftUI
 import Firebase
 import GoogleSignIn
 import SDWebImageSwiftUI
+import CryptoKit
+import FirebaseAuth
+import AuthenticationServices
+
 
 struct AuthViewManager: View {
     @EnvironmentObject var viewModel: AuthViewModel
@@ -21,17 +25,63 @@ struct AuthViewManager: View {
 
 struct SignInView: View {
     @EnvironmentObject var viewModel: AuthViewModel
+    @State var currentNonce:String?
+
     var body: some View {
         Button(action: {
             viewModel.signIn()
         }) {
-            HStack {
-                Image("google")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 35, height: 35)
-                Text("Sign in with Google")
-            }
+            SignInWithAppleButton(
+                
+                //Request
+                onRequest: { request in
+                    let nonce = viewModel.randomNonceString()
+                    currentNonce = nonce
+                    request.requestedScopes = [.fullName, .email]
+                    request.nonce = viewModel.sha256(nonce)
+                },
+                
+                //Completion
+                onCompletion: { result in
+                    switch result {
+                    case .success(let authResults):
+                        switch authResults.credential {
+                        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                            
+                            guard let nonce = currentNonce else {
+                                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                            }
+                            guard let appleIDToken = appleIDCredential.identityToken else {
+                                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                            }
+                            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                                return
+                            }
+                            
+                            let credential = OAuthProvider.credential(withProviderID: "apple.com",idToken: idTokenString,rawNonce: nonce)
+                            Auth.auth().signIn(with: credential) { (authResult, error) in
+                                if (error != nil) {
+                                    // Error. If error.code == .MissingOrInvalidNonce, make sure
+                                    // you're sending the SHA256-hashed nonce as a hex string with
+                                    // your request to Apple.
+                                    print(error?.localizedDescription as Any)
+                                    return
+                                }
+                                print("signed in")
+                            }
+                            
+                            print("\(String(describing: Auth.auth().currentUser?.uid))")
+                        default:
+                            break
+                            
+                        }
+                    default:
+                        break
+                    }
+                    
+                }
+            ).frame(width: 280, height: 45, alignment: .center)
         }
     }
 }
@@ -57,12 +107,16 @@ struct SignedInView: View {
     }
 }
 
+
 class AuthViewModel: ObservableObject {
+    
     enum SignInState {
         case signedIn
         case signedOut
     }
     @Published var state: SignInState = .signedOut
+    
+    // Google Sign In
     
     func signIn() {
         if GIDSignIn.sharedInstance.hasPreviousSignIn() {
@@ -94,7 +148,7 @@ class AuthViewModel: ObservableObject {
         }
     }
     
-    private func authenticateUser(for user: GIDGoogleUser?, with err: Error?) {
+    func authenticateUser(for user: GIDGoogleUser?, with err: Error?) {
         if let err = err {
             print(err.localizedDescription)
             return
@@ -122,8 +176,55 @@ class AuthViewModel: ObservableObject {
                         print("Tell us about yourself")
                     }
                 }
-
+                
             }
         }
     }
+    
+    // Apple Sign In
+    
+    func sha256(_ input: String) -> String {
+            let inputData = Data(input.utf8)
+            let hashedData = SHA256.hash(data: inputData)
+            let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+            }.joined()
+
+            return hashString
+        }
+    
+    func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    
+    
 }
