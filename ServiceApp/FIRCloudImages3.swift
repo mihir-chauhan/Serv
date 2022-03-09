44cca49ae701ae6fe3723fc5f5cf61aa6062d9ee
Kelvin <63611619+kelvinjou@users.noreply.github.com>
2022-03-08 19:00:10 -0800

    NSCaching is now working

--- ServiceApp/FIRCloudImages3.swift
+++ ServiceApp/FIRCloudImages3.swift
@@ -11,15 +11,13 @@ import SwiftUI
 
 class FIRCloudImages3 {
     static let storage = Storage.storage()
-    static let cache = NSCache<NSString, UIImage>()
+    static let cache = NSCache<NSString, NSData>()
     
     static func getRemoteImages(gsURL: String, completion: @escaping ((UIImage)?) -> ()) {
         let storageRef = storage.reference().child("EventImages")
         
         storageRef.listAll { (result, error) in
                 for item in result.items {
-                    //                        get image then convert to data for cache use existing
-                    
                     item.getData(maxSize: 1 * 1024 * 1024 * 1024, completion: { data, error in
                         if let err = error {
                             fatalError(err.localizedDescription)
@@ -27,12 +25,9 @@ class FIRCloudImages3 {
                         else if gsURL.contains(item.fullPath) {
                             let downloadedImage = UIImage(data: data!)
                             
-                            if downloadedImage != nil {
-                                self.cache.setObject(downloadedImage!, forKey: gsURL as NSString)
-                            }
-//                            if let image = self.cache.object(forKey: gsURL as NSString) {
-//                                print("CACHEDDD")
-//                            }
+                        
+                            self.cache.setObject(data! as NSData, forKey: gsURL as NSString)
+                            
                             completion(downloadedImage)
 
                         }
@@ -43,13 +38,21 @@ class FIRCloudImages3 {
     }
         
     static func getImage(gsURL: String, completion: @escaping ((UIImage)?) -> ()) {
-        if let image = cache.object(forKey: gsURL as NSString) {
+        if let imageData = cache.object(forKey: gsURL as NSString) {
             print("cached results")
+            
+            let image = UIImage(data: imageData as Data)
             completion(image)
         }
         else {
             getRemoteImages(gsURL: gsURL, completion: completion)
             print("loading new results")
+            
+//            if let all = cache.value(forKey: gsURL) as? NSArray {
+//                for object in all {
+//                    print("object is \(object)")
+//                }
+//            }
         }
     }
 }
