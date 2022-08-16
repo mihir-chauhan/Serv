1323241115613b5bea0ada49ab60a5b1b4b58a1c
Mihir Chauhan <mihiravikam@gmail.com>
2022-08-16 14:26:04 -0700

    Need to add FirebaseMessaging; see next commit

--- ServiceApp/ServiceAppApp.swift
+++ ServiceApp/ServiceAppApp.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import Firebase
+import FirebaseMessaging
+import FirebaseAnalytics
 import GoogleSignIn
 
 @main
@@ -23,10 +25,47 @@ struct ServiceAppApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
+        
+        // Register Notifications
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions) { _, _ in }
+
+        application.registerForRemoteNotifications()
+
         return true
     }
     
     func application(_ application: UIApplication, open url: URL, option: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
         return GIDSignIn.sharedInstance.handle(url)
     }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+  func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    willPresent notification: UNNotification,
+    withCompletionHandler completionHandler:
+    @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
+    completionHandler([[.banner, .sound]])
+  }
+
+  func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    didReceive response: UNNotificationResponse,
+    withCompletionHandler completionHandler: @escaping () -> Void
+  ) {
+    completionHandler()
+  }
+}
+
+func application(
+  _ application: UIApplication,
+  didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+) {
+  Messaging.messaging().apnsToken = deviceToken
 }
