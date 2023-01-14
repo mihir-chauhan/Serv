016b4eed2ff271a322dabddff7be9c25d9d6d0ed
Kelvin <63611619+kelvinjou@users.noreply.github.com>
2023-01-14 15:25:16 -0800

    WIP graph indicator

--- ServiceApp/NEWGRAPH.swift
+++ ServiceApp/NEWGRAPH.swift
@@ -18,49 +18,61 @@ struct LineGraph2: View {
     @State var translation: CGFloat = 0
     var body: some View {
         let incrementedValData: [CGFloat] = addNewValueToPrevious()
-        RoundedRectangle(cornerRadius: 20)
-            .frame(width: UIScreen.main.bounds.width - 30, height: 275)
-            .foregroundColor(Color(#colorLiteral(red: 0.5294117647, green: 0.6705882353, blue: 0.9843137255, alpha: 0.25)))
-            .overlay(
-                GeometryReader { geometry in
-                    Path { path in
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: UIScreen.main.bounds.width - 30, height: 275)
+                .foregroundColor(Color(#colorLiteral(red: 0.5294117647, green: 0.6705882353, blue: 0.9843137255, alpha: 0.25)))
+                .overlay(
+                    GeometryReader { geometry in
                         let width = geometry.size.width
                         let height = geometry.size.height
-                        let minValue = incrementedValData.min() ?? 0
-                        let maxValue = incrementedValData.max() ?? 1
-                        let points = incrementedValData.enumerated().map { x, y in
-                            CGPoint(x: CGFloat(x) * width / CGFloat(self.data.count - 1),
-                                    y: (height - ((y - minValue) * height) / (maxValue - minValue)) )
-                        }
-                        path.move(to: points[0])
-                        for point in points.dropFirst() {
-                            path.addLine(to: point)
-                        }
-                    }
-                    .stroke(Color.green, lineWidth: 2)
-                }
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    .gesture(DragGesture()
-                        .onChanged({ value in
-                            withAnimation { showPlot = true }
-                            let translation = value.location.x - 40
-                            
-                            let index = max(min(Int((translation / UIScreen.main.bounds.width - 30).rounded() + 1), data.count - 1), 0)
-                            
-                            if !data.isEmpty {
-                                print("AHHH", translation)
-                                //                                   currentPlot = String(format: "%.2f", prevProgess + data[index])
-                                //                                   self.translation = translation
+                        ZStack {
+                            Path { path in
                                 
-                                //                                   offset = CGSize(width: points[index].x - 40, height: points[index].y - height)
+                                let minValue = incrementedValData.min() ?? 0
+                                let maxValue = incrementedValData.max() ?? 1
+                                let points = incrementedValData.enumerated().map { x, y in
+                                    CGPoint(x: CGFloat(x) * width / CGFloat(self.data.count - 1),
+                                            y: (height - ((y - minValue) * height) / (maxValue - minValue)) )
+                                }
+                                path.move(to: points[0])
+                                for point in points.dropFirst() {
+                                    path.addLine(to: point)
+                                }
                             }
-                            
-                        })
-                            .onEnded({ value in
-                                withAnimation { showPlot = false }
+                            .stroke(Color.green, lineWidth: 2)
+                        }
+                        Circle()
+                            .frame(width: 20, height: 20)
+                            .position(x: width - 30, y: height - 30)
+                            .foregroundColor(.white)
+                        //                                            .background(Color.red)
+                            .offset(x: -10, y: -10)
+                            .opacity(0.7)
+                    }
+
+                        .gesture(DragGesture()
+                            .onChanged({ value in
+                                withAnimation { showPlot = true }
+                                let translation = value.location.x - 40
+                                
+                                let index = max(min(Int((translation / UIScreen.main.bounds.width - 30).rounded() + 1), data.count - 1), 0)
+                                
+                                if !data.isEmpty {
+                                    print("AHHH", translation)
+                                    //                                   currentPlot = String(format: "%.2f", prevProgess + data[index])
+                                    //                                   self.translation = translation
+                                    
+                                    //                                   offset = CGSize(width: points[index].x - 40, height: points[index].y - height)
+                                }
+                                
                             })
-                    )
+                                .onEnded({ value in
+                                    withAnimation { showPlot = false }
+                                })
+                        )
             )
+        }
     }
     
     func addNewValueToPrevious() -> [CGFloat] {
@@ -81,6 +93,13 @@ struct LineGraph2: View {
         
         print("final array ", newArray)
         return newArray
+    }
+    
+    @ViewBuilder
+    func buildIndicator() -> some View {
+        Capsule()
+            .frame(width: 20, height: 10)
+            .offset()
     }
 }
 
